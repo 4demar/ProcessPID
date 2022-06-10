20b234cfecdca1bab07501b68dd8b5f8ad9e7f37
Ademar Junior <94468763+4demar@users.noreply.github.com>
2022-06-10 15:57:11 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var singleton = Singleton.ClassSingleton.GetInstancia;
+            IniciarProcesso iniciar = new IniciarProcesso();
+            for (int i = 0; i <= 2; i++)
+            {
+                iniciar.IniciarPaint(i);
+            }
+
+            Thread.Sleep(TimeSpan.FromSeconds(20)); // 30 Segundos
+
+            Process[] processAberto = Process.GetProcessesByName("mspaint");
+            List<int> processoAtual = new List<int>();
+            for (int i = 0; i < processAberto.Length; i++)
+            {
+                processoAtual.Add(processAberto[i].Id);
+            }
+
+            //// Compara e mostra na tela...
+            var listaCaiu = singleton.numeroProcess.Except(processoAtual).ToList();
+
+            for (int i = 0; i < listaCaiu.Count; i++)
+            {
+                Console.WriteLine("Paint de PID: " + listaCaiu[i] + " fechou...");
+                foreach(IProcesso dados in singleton.Processo)
+                {
+                    if (dados.numeroPID == listaCaiu[i])
+                    {
+                        Console.WriteLine("Paint: " + dados.nomeProcesso + " fechou...");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
