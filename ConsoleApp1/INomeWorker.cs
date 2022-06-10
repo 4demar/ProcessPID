30a1b12006de21a475ed25a8ab2cbc154679bab2
Ademar Junior <94468763+4demar@users.noreply.github.com>
2022-06-10 15:40:42 -0300

    Add files via upload

---
+++ ConsoleApp1/INomeWorker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class INomeWorker
+    {
+        public int numeroPID { get; set; }
+        public int numeroWorker { get; set; }
+    }
+}
